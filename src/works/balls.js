4f1c6cf2c1258bd40590ace370834c9c4c113e44
maxiangyu <maxiangyu01@baidu.com>
2018-09-19 13:13:06 +0800

    feat: 彩球

---
+++ src/works/balls.js
@@ -0,0 +1,52 @@
+let balls = [];
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+import drawBackground from '../utils/drawGrid';
+
+for (let i = 0; i < 50; i++) {
+    balls[i] = {
+        x: canvas.width / 2,
+        y: canvas.height / 2,
+        radius: 30 * Math.random(),
+        velocityX: 3 * Math.random(),
+        velocityY: 3 * Math.random(),
+        color: `rgb(
+                ${(255 * Math.random()).toFixed(0)},
+                ${(255 * Math.random()).toFixed(0)},
+                ${(255 * Math.random()).toFixed(0)})`
+    };
+}
+
+
+function drawBalls() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground(context);
+
+    balls.forEach(ball => {
+        context.beginPath();
+        context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+        context.fillStyle = ball.color;
+        context.fill();
+
+        moveBall(ball);
+    });
+
+    requestAnimationFrame(drawBalls);
+}
+
+function moveBall(ball) {
+    if (ball.x + ball.velocityX + ball.radius > canvas.width ||
+        ball.x + ball.velocityX - ball.radius < 0) {
+        ball.velocityX = -ball.velocityX;
+    }
+
+    if (ball.y + ball.velocityY + ball.radius > canvas.height ||
+        ball.y + ball.velocityY - ball.radius < 0) {
+        ball.velocityY = -ball.velocityY;
+    }
+
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+}
+
+requestAnimationFrame(drawBalls);
